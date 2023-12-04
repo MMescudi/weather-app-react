6426aeffa7435d85dfa40c6ee7210f1e67036c3d
MMescudi <michelle.dachs@hotmail.de>
2023-12-04 17:41:56 +0100

    weather project transferred

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The Weather in ${response.data.name} ${response.data.main.temp} °C`);
-  }
-  let apiKey = "e766b056eefc0cbfcf74d994e0e180b7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather React</h2>;
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e766b056eefc0cbfcf74d994e0e180b7&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter your City"
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>
+            {" "}
+            <strong>{weather.description}</strong>
+          </li>
+          <li>
+            Temperature: <strong>{Math.round(weather.temperature)}</strong>°C
+          </li>
+          <li>
+            Humidity: <strong>{Math.round(weather.humidity)}</strong>%
+          </li>
+          <li>
+            Wind: <strong>{Math.round(weather.wind)}</strong>
+            Km/h
+          </li>
+          <li>
+            <img src={weather.icon} alt="Weather Icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
